7c86d155b1ce4350c98997056c6a95693447e73e
linh1509 <66712896+linh1509@users.noreply.github.com>
2020-07-01 17:13:45 +0700

    Update 06.03.00.mvc01.js

--- 06.03.00.mvc01.js
+++ 06.03.00.mvc01.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 var session = require('express-session')
 var path = require('path');
 var router = express.Router();  
@@ -256,4 +256,4 @@ function viewReview(request, response) {
 /// ***************** ***************** *****************
 /// ***************** ***************** *****************
 /// ***************** ***************** *****************
-app.listen(port, () => console.log(`\n\tWeb app listening at http://localhost:${port}`));
+app.listen(PORT, () => console.log(`\n\tWeb app listening at http://localhost:${PORT}`));
